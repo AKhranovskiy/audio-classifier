f6c36d654b98ec192d3fa606f48f0c733c696412
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2022-11-25 12:29:19 +0700

    refactor verify

--- src/py/verify.py
+++ src/py/verify.py
@@ -3,15 +3,33 @@ import pickle
 import sys
 import math
 import numpy as np
-import tensorflow as tf
 from tensorflow import keras
 
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 INPUT_SHAPE = [150, 39, 1]
 CLASSES = 3
 
-def verify(data):
+def verify(data, labels, classes):
+    assert data.shape[0] == labels.shape[0]
+    assert list(data.shape[1:]) == INPUT_SHAPE
+
+    print("Verifying {} images.".format(data.shape[0]))
+
+    print("Loading the model")
+    model = keras.models.load_model("./model")
+
+    y_pred = model(data)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    labels = keras.utils.to_categorical(labels, num_classes=classes)
+    y_test = np.argmax(labels, axis=1)
+
+    return np.sum(y_pred == y_test) / len(y_pred)
+
+
+if __name__ == "__main__":
+    data = sys.argv[1]
     print("Loading data from " + data + "... ")
 
     mfccs = []
@@ -23,36 +41,10 @@ def verify(data):
     aligned_length = math.ceil(max_length / chunk) * chunk
     mfccs = list(map(lambda x: x + x[-aligned_length + len(x) :], mfccs))
 
-    mfccs = np.array(mfccs)
-    mfccs = mfccs.reshape(mfccs.shape[0], -1, *INPUT_SHAPE)
+    mfccs = np.array(mfccs).reshape(-1, *INPUT_SHAPE)
 
-    labels = np.array([[idx] * v.shape[0] for (idx, v) in enumerate(mfccs)])
-
-    x = np.vstack(mfccs)
-    y = np.hstack(labels)
-
-    y = tf.keras.utils.to_categorical(y, num_classes=CLASSES)
-
-    print("Data is loaded. images: ", x.shape[0])
-    print("Verifying...")
-
-    input_shape = x.shape[1:]
-
-    model = keras.models.load_model("./model")
-
-    @tf.function
-    def predict(inputs):
-        return model(inputs)
-
-    predict_fn = predict.get_concrete_function(
-        tf.TensorSpec(shape=[1, *INPUT_SHAPE], dtype=tf.float32, name="inputs")
-    )
-
-    y_pred = predict(x)
-    y_pred = np.argmax(y_pred, axis=1)
-    y_test = np.argmax(y, axis=1)
-
-    return np.sum(y_pred == y_test) / len(y_pred)
-
-if __name__ == "__main__":
-    verify()
+    label_len = len(mfccs) // CLASSES
+    
+    labels = np.array([0,1,2]).repeat(label_len)
+    print(mfccs.shape, labels.shape)
+    print("Accuracy ", verify(mfccs, labels, CLASSES))
