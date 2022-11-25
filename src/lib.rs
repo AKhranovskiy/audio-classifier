1bb49bd586aa3880b2a54c8bce4b967cbabdf180
Andrey Khranovsky <andrey.khranovsky@gmail.com>
2022-11-25 13:44:40 +0700

    implement verify on rust side

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use pyo3::PyResult;
-
 #[non_exhaustive]
 pub struct Classifier;
 
@@ -12,7 +10,7 @@ pub fn init() -> &'static Classifier {
 // SHAPE 150x39
 
 impl Classifier {
-    pub fn hello(&self) -> PyResult<()> {
+    pub fn hello(&self) -> anyhow::Result<()> {
         use pyo3::prelude::*;
         use pyo3::types::IntoPyDict;
 
@@ -29,14 +27,46 @@ impl Classifier {
         })
     }
 
-    pub fn verify(&self) -> PyResult<f64> {
+    pub fn train_batch(&self) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    pub fn verify(&self) -> anyhow::Result<f64> {
+        let f = std::fs::File::open("./data.pickle")?;
+        let reader = std::io::BufReader::new(f);
+
+        let data: Vec<Vec<f64>> = serde_pickle::from_reader(reader, Default::default())?;
+        assert_eq!(3, data.len());
+
+        let min_len = data.iter().map(Vec::len).min().unwrap();
+        assert_eq!(0, min_len % (150 * 39));
+
+        let data = data
+            .into_iter()
+            .flat_map(|v| v.into_iter().take(min_len))
+            .collect::<Vec<_>>();
+
+        let number_of_images = data.len() / (150 * 39);
+        dbg!(&number_of_images);
+
+        let data = ndarray::Array4::from_shape_vec((number_of_images, 150, 39, 1), data)?;
+        dbg!(data.shape());
+
+        let labels = ndarray::concatenate![
+            ndarray::Axis(0),
+            ndarray::Array1::from_elem((number_of_images / 3,), 0),
+            ndarray::Array1::from_elem((number_of_images / 3,), 1),
+            ndarray::Array1::from_elem((number_of_images / 3,), 2)
+        ];
+
+        use numpy::IntoPyArray;
         use pyo3::prelude::*;
 
         Python::with_gil(|py| {
             let result: f64 =
                 PyModule::from_code(py, include_str!("py/verify.py"), "verify.py", "verify")?
                     .getattr("verify")?
-                    .call1(("./data.pickle",))?
+                    .call1((data.into_pyarray(py), labels.into_pyarray(py), 3))?
                     .extract()?;
             Ok(result)
         })
